0d863c1f1adfde702fba028dff60264fc95c8f6b
Orivionceo <claytontoppin@gmail.com>
2026-06-16 15:13:58 -0700

    Add frontend App component

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>ORIVION Dashboard</h1>
+      <p>Welcome to ORIVION! More features coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
